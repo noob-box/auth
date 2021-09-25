7b8160f186c77f60532ab9b0ac57ca609d503555
Mark Hähnel <hello@markhaehnel.de>
2021-09-25 13:30:53 +0200

    Add e2e tests, add docker related stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseService } from './database/database.service';
 import { UserService } from './user/user.service';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './auth/roles.guard';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthModule } from './auth/auth.module';
+import { SupertokensExceptionFilter } from './auth/auth.filter';
 
 @Module({
   imports: [AuthModule],
@@ -15,6 +16,10 @@ import { AuthModule } from './auth/auth.module';
     AppService,
     DatabaseService,
     UserService,
+    {
+      provide: APP_FILTER,
+      useClass: SupertokensExceptionFilter,
+    },
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,21 +2,37 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { AuthModule } from './../src/auth/auth.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule, AuthModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/sessioninfo').expect(HttpStatus.UNAUTHORIZED);
+  const authSignInPath = '/auth/signin';
+
+  describe(authSignInPath, () => {
+    it('POST - Empty body', () => {
+      return request(app.getHttpServer()).post(authSignInPath).expect(HttpStatus.BAD_REQUEST);
+    });
+
+    it('POST - Non existing user body', () => {
+      return request(app.getHttpServer())
+        .post(authSignInPath)
+        .send({
+          formFields: [
+            { id: 'email', value: 'test@example.com' },
+            { id: 'password', value: 'password123' },
+          ],
+        })
+        .expect(HttpStatus.OK)
+        .expect('{"status":"WRONG_CREDENTIALS_ERROR"}');
+    });
   });
 });
