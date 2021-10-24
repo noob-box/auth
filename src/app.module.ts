02b4c77478c2fe1cf01dad3e0a324272db6bb33d
Mark Hähnel <mark.haehnel@gmail.com>
2021-10-24 11:53:10 +0200

    Add health controller and prisma healthcheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { AdminModule } from './admin/admin.module';
 import { ConfigModule } from '@nestjs/config';
 import { getValidatedConfiguration } from './config/configuration';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { getValidatedConfiguration } from './config/configuration';
     AuthModule,
     UsersModule,
     AdminModule,
+    HealthModule,
   ],
   controllers: [AppController],
 })
