86a86838aca6cbb057efb46f67a50a51795470e4
Mark Hähnel <hello@markhaehnel.de>
2021-10-27 21:13:10 +0200

    Fix test regex names

--- test/utils/regex.ts
+++ test/utils/regex.ts
@@ -1,4 +1,4 @@
-const containsJwtRegex = /(^(?:[\w-]*\.){2}[\w-]*$)/;
-const jwtRegex = /(?:[\w-]*\.){2}[\w-]*/;
+const jwtRegex = /(^(?:[\w-]*\.){2}[\w-]*$)/;
+const containsJwtRegex = /(?:[\w-]*\.){2}[\w-]*/;
 
 export { jwtRegex, containsJwtRegex };
